b2499ee86352f5a867b9b14262fdfcc7125a5a4f
manaskumar2020 <47380652+manaskumar2020@users.noreply.github.com>
2023-09-26 20:32:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import logging
 import logging.handlers
 import os
-
+import subprocess
 import requests
 
 logger = logging.getLogger(__name__)
@@ -26,12 +26,28 @@ except KeyError:
 
 if __name__ == "__main__":
     logger.info(f"Token value: {SOME_SECRET}")
-
     r = requests.get('https://cat-fact.herokuapp.com/facts/')
+    # Email configuration
+    recipient = "manas.kumar@capgemini.com"
+    subject = "Hello from Python #CGO#"
+    message = "This is a test email sent from Python using mailx."
+
+    # Construct the email body
+    email_body = f"Subject: {subject}\n\n{message}"
+
+    # Use the `subprocess` module to send the email
+    try:
+        p = subprocess.Popen(["mailx", "-s", subject, recipient], stdin=subprocess.PIPE)
+        p.communicate(input=email_body.encode())
+        p.wait()
+        print("Email sent successfully")
+    except Exception as e:
+        print("An error occurred:", str(e))
+
     
     if r.status_code == 200:
         data = r.json()
         logger.info(data)
-        print(data)
+        # print(data)
         # temperature = data["forecast"]["temp"]
         logger.info(f'Weather in Berlin:')
